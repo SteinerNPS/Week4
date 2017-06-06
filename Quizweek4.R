8ef91e0da63d87163c14f7e7a2b6ba191c3c37d0
SteinerNPS <michael_steiner@nps.gov>
2017-06-05 22:14:49 -0700

    Add files via upload

--- Quizweek4.R
+++ Quizweek4.R
@@ -102,10 +102,36 @@ rankhospital <- function(state, outcome_name, num = "best"){
     sub_outcome <- sub_outcome[order(sub_outcome[[x]]),]
     as.character(sub_outcome$Hospital.Name[num])
   }
-  
-  
 }
 
+rankall <- function(outcome_name, num = "best") {
+  outcome <- read.csv("outcome-of-care-measures.csv")
+  outcome_types <- c('heart attack', 'heart failure', 'pneumonia')
+  if(!outcome_name %in% outcome_types){
+    stop("invalid outcome")
+  }
+  foutcome_name <- strsplit(outcome_name, " ")[[1]]
+  if(length(strsplit(foutcome_name, " ")) > 1){
+    temp_name <- paste(toupper(substr(foutcome_name[1],1,1)), substr(foutcome_name[[1]],2, nchar(foutcome_name[[1]])), sep ='')
+    temp_name <- paste(temp_name, paste(toupper(substr(foutcome_name[[2]],1,1)), substr(foutcome_name[[2]],2,nchar(foutcome_name[[2]])), sep = ''), sep = ".")
+    outcome_name <- temp_name  
+  }else if (length(strsplit(foutcome_name, " ")) == 1){
+    outcome_name = paste(toupper(substr(foutcome_name[1],1,1)), substr(foutcome_name[[1]],2, nchar(foutcome_name[[1]])), sep ='')
+  }
+  x <- paste("Hospital.30.Day.Death..Mortality..Rates.from.", outcome_name, sep = '')
+  if(num == 'best'){num <- 1}
+  if(num == "worst"){
+    sub_outcome[[x]] <- as.numeric(sub_outcome[[x]])
+    max_death <- max(sub_outcome[[x]])
+    sub_outcome <-sub_outcome[sub_outcome[[x]] == max_death,]
+    num = nrow(sub_outcome)
+  }
+  sub_outcome <- outcome
+  sub_outcome[[x]] <- as.numeric(sub_outcome[[x]])
+  sub_outcome <- unique.data.frame(sub_outcome)
+  sub_outcome <- sub_outcome[order(sub_outcome[[x]]),]
+  as.character(sub_outcome$Hospital.Name[num])
+}
 
 
 
